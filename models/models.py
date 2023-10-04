d0ffbe8e359fd1ef061714e9c3aba9825e52fb80
NikolayZemelko <zemelko1989@gmail.com>
2023-10-04 21:04:08 +0300

    Add database auth, change models

--- models/models.py
+++ models/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from sqlalchemy import (MetaData, Table, Column, Boolean,
+                        Integer, String, TIMESTAMP, ForeignKey, JSON)
 
 metadata = MetaData()
 
-roles = Table(
-    "roles",
+role = Table(
+    "role",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
@@ -13,13 +14,17 @@ roles = Table(
 )
 
 
-users = Table(
-    "users",
+user = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
     Column("password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column("hashed_password", String(length=1024), nullable=False),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
